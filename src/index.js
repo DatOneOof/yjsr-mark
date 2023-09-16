e2bd74a23df88f55a18da453c6c1ad06e1286684
Rayane Nossair <rnossairx@gmail.com>
2023-09-16 15:21:29 +0200

    added vercel analytics

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Analytics } from '@vercel/analytics/react';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Analytics/>
     <App />
   </React.StrictMode>
 );
